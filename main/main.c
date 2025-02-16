3c7f69bf4b0e7ac60602ce99d50861df97b08256
Shaun Hey <shaun@shaunhey.com>
2025-02-16 20:22:02 +0000

    Clean up

--- main/main.c
+++ main/main.c
@@ -7,11 +7,24 @@
 #include "mqtt_client.h"
 #include "nvs_flash.h"
 
+#define SSID_MAX_LEN 32
+#define WIFI_CONNECTED_BIT BIT0
+
 static const char *TAG = "main";
 static esp_mqtt_client_handle_t s_mqtt_client;
 static EventGroupHandle_t s_wifi_event_group;
 
-#define WIFI_CONNECTED_BIT BIT0
+typedef struct
+{
+    int16_t frame_cntl;
+    int16_t duration;
+    uint8_t da[6];
+    uint8_t sa[6];
+    uint8_t bssid[6];
+    int16_t seq_cntl;
+    uint8_t payload[];
+
+} __attribute__((packed)) wifi_mgmt_pkt_t;
 
 static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
@@ -36,20 +49,6 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t e
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
-
-typedef struct
-{
-    int16_t frame_cntl;
-    int16_t duration;
-    uint8_t da[6];
-    uint8_t sa[6];
-    uint8_t bssid[6];
-    int16_t seq_cntl;
-    uint8_t payload[];
-
-} __attribute__((packed)) wifi_mgmt_pkt_t;
-
-#define SSID_MAX_LEN 32
 
 static void mgmt_pkt_cb(wifi_promiscuous_pkt_t *prom_pkt)
 {
@@ -104,13 +103,25 @@ static void start_mqtt()
     esp_mqtt_client_publish(s_mqtt_client, CONFIG_MQTT_TOPIC "/status", "started", 0, 1, false);
 }
 
-void start_ntp()
+static void start_ntp()
 {
     ESP_LOGI(TAG, "Configuring ntp...");
     esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_NTP_SERVER);
     esp_netif_sntp_init(&config);
     ESP_ERROR_CHECK(esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)));
     ESP_LOGI(TAG, "Time set!");
+}
+
+static void start_nvs()
+{
+    ESP_LOGI(TAG, "Initializing non-volatile storage");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 }
 
 static void start_wifi()
@@ -128,21 +139,18 @@ static void start_wifi()
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_country_code("US", true));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
     
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_event_handler,
                                                         NULL,
-                                                        &instance_any_id));
+                                                        NULL));
 
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
                                                         &wifi_event_handler,
                                                         NULL,
-                                                        &instance_got_ip));
+                                                        NULL));
 
     wifi_config_t sta_wifi_config = {
         .sta = {
@@ -173,15 +181,7 @@ void app_main(void)
     ESP_LOGI(TAG, "Starting");
     s_wifi_event_group = xEventGroupCreate();
 
-    ESP_LOGI(TAG, "Initializing non-volatile storage");
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
+    start_nvs();
     start_wifi();
     start_ntp();
     start_mqtt();
